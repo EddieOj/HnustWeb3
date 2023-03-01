92675819cf783c3f7aa79286d2d0107e1fc20a22
Lixna <2038659229@qq.com>
2023-03-01 09:50:50 +0800

    version3

--- src/main/java/com/lixna/sgninsys/controller/ClassController.java
+++ src/main/java/com/lixna/sgninsys/controller/ClassController.java
@@ -5,7 +5,6 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.lixna.sgninsys.common.R;
 import com.lixna.sgninsys.entity.Class;
 import com.lixna.sgninsys.entity.User;
-import com.lixna.sgninsys.entity.vo.ClassDto;
 import com.lixna.sgninsys.service.ClassService;
 import com.lixna.sgninsys.service.UserService;
 import io.swagger.annotations.Api;
@@ -19,7 +18,7 @@ import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author lixna
@@ -35,46 +34,44 @@ public class ClassController {
     ClassService classService;
     @Resource
     UserService userService;
+
     //新增班级
     @ApiOperation(value = "新增班级")
     @PostMapping("/save")
-    public R save(@RequestBody ClassDto classDto){
-        Class newClass = new Class();
-        newClass.setClassCapacity(classDto.getClass_capacity());
-        newClass.setClassName(classDto.getClass_name());
-        newClass.setCreateId(classDto.getCreator());
+    public R save(@RequestBody Class newClass) {
         boolean isSaved = classService.save(newClass);
-        if (isSaved){
+        if (isSaved) {
             return R.ok();
-        }else {
+        } else {
             return R.error();
         }
     }
+
     //查询所有创建的课程或参加的课程
     @ApiOperation(value = "根据用户Id查询所有班级")
     @GetMapping("/findAllByUserId/{userId}")
-    public R listAll(@ApiParam(name = "userId", value = "用户ID", required = true)@PathVariable Integer userId,
-                     Integer tabIndex){
+    public R listAll(@ApiParam(name = "userId", value = "用户ID", required = true) @PathVariable Integer userId,
+                     Integer tabIndex) {
         QueryWrapper<Class> wrapper = new QueryWrapper<>();
-        if(tabIndex == 0){
-            wrapper.eq("create_id",userId);
+        if (tabIndex == 0) {
+            wrapper.eq("create_id", userId);
             List<Class> list = classService.list(wrapper);
-            return R.ok().data("items",list);
-        }else{
-            wrapper.eq("class_id",userService.getById(userId).getClassId());
+            return R.ok().data("items", list);
+        } else {
+            wrapper.eq("class_id", userService.getById(userId).getClassId());
             List<Class> list = classService.list(wrapper);
-            return R.ok().data("items",list);
+            return R.ok().data("items", list);
         }
     }
 
     //删除班级
     @ApiOperation(value = "根据班级ID删除班级")
     @GetMapping("/deleteClassById/{id}")
-    public R deleteprojectById(@ApiParam(name = "id", value = "班级ID", required = true)@PathVariable Integer id){
+    public R deleteprojectById(@ApiParam(name = "id", value = "班级ID", required = true) @PathVariable Integer id) {
         boolean isDeleted = classService.removeById(id);
-        if(isDeleted){
+        if (isDeleted) {
             return R.ok();
-        }else{
+        } else {
             return R.error();
         }
     }
@@ -82,34 +79,35 @@ public class ClassController {
     //根据id查询，用于信息回显
     @ApiOperation(value = "根据id查询")
     @GetMapping("/getById/{id}")
-    public R getById(@PathVariable Integer id){
+    public R getById(@PathVariable Integer id) {
         Class newClass = classService.getById(id);
-        return R.ok().data("item",newClass);
+        return R.ok().data("item", newClass);
     }
 
     //修改班级
     @ApiOperation(value = "修改班级")
     @PostMapping("/updateById")
-    public R updateById(@RequestBody Class newClass){
+    public R updateById(@RequestBody Class newClass) {
         boolean flag = classService.updateById(newClass);
-        if (flag){
+        if (flag) {
             return R.ok();
-        }else {
+        } else {
             return R.error();
         }
     }
 
-    @ApiOperation(value = "根据用户ID删除班级")
+    @ApiOperation(value = "退出班级")
     @GetMapping("/out/{userId}")
-    public R quitClass(@ApiParam(name = "userId", value = "用户ID", required = true) @PathVariable Integer userId){
-        User user =  userService.getById(userId);
+    public R quitClass(@ApiParam(name = "userId", value = "用户ID", required = true) @PathVariable Integer userId) {
+        User user = userService.getById(userId);
         user.setClassId(-1);
         boolean isUpdated = userService.updateById(user);
-        if(isUpdated){
+        if (isUpdated) {
             return R.ok();
-        }else{
+        } else {
             return R.error();
         }
     }
+
 }
 
--- src/main/java/com/lixna/sgninsys/controller/UserController.java
+++ src/main/java/com/lixna/sgninsys/controller/UserController.java
@@ -1,9 +1,19 @@
 package com.lixna.sgninsys.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.lixna.sgninsys.common.R;
+import com.lixna.sgninsys.entity.User;
+import com.lixna.sgninsys.service.ClassService;
+import com.lixna.sgninsys.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -15,7 +25,35 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/sgninsys/user")
+@Slf4j
+@CrossOrigin
+@Api(tags = "用户模块")
 public class UserController {
 
+    @Resource
+    ClassService classService;
+    @Resource
+    UserService userService;
+    //查询班级所有人数
+    @ApiOperation(value = "根据班级Id查询所有成员")
+    @GetMapping("/findAllByClassId/{classId}")
+    public R listAllPeople(@ApiParam(name = "classId", value = "班级ID", required = true) @PathVariable Integer classId) {
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.eq("class_id", classId);
+        List<User> list = userService.list(wrapper);
+        return R.ok().data("items", list);
+    }
+
+    //删除用户
+    @ApiOperation(value = "根据用户ID删除用户")
+    @GetMapping("/deleteUserById/{id}")
+    public R deleteUserById(@ApiParam(name = "id", value = "用户ID", required = true) @PathVariable Integer id) {
+        boolean isDeleted = userService.removeById(id);
+        if (isDeleted) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+    }
 }
 
